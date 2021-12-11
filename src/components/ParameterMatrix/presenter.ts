636864b2278e464a466dc4038ee3a47af1d8ea76
Ragi <ragi-justaway@hotmail.co.jp>
2021-12-11 18:01:48 +0900

    Add parameterRepository

--- src/components/ParameterMatrix/presenter.ts
+++ src/components/ParameterMatrix/presenter.ts
@@ -1,3 +1,4 @@
+import { getUserParameter } from '../../infrastructures/dummy/parameterRepository';
 import { Presenter } from '../../utils/connect';
 import { ParameterMatrixViewProps } from './view';
 
@@ -7,25 +8,5 @@ export const ParameterMatrixPresenter: Presenter<
   Props,
   ParameterMatrixViewProps
 > = ({}: Props) => {
-  return {
-    parameters: {
-      STR: { self: 10, mod: 0, tmp: 0 },
-      CON: { self: 11, mod: 0, tmp: 0 },
-      POW: { self: 12, mod: 0, tmp: 0 },
-      DEX: { self: 13, mod: 0, tmp: 0 },
-      APP: { self: 14, mod: 0, tmp: 0 },
-      SIZ: { self: 15, mod: 0, tmp: 0 },
-      INT: { self: 16, mod: 0, tmp: 0 },
-      EDU: { self: 17, mod: 0, tmp: 0 },
-      INC: { self: 18, mod: 0, tmp: 0 },
-    },
-    extraParams: {
-      HP: { self: 12, mod: 0, tmp: 0 },
-      MP: { self: 13, mod: 0, tmp: 0 },
-      SAN: { self: 14, mod: 0, tmp: 0 },
-      IDE: { self: 15, mod: 0, tmp: 0 },
-      LUK: { self: 16, mod: 0, tmp: 0 },
-      KNOW: { self: 17, mod: 0, tmp: 0 },
-    },
-  };
+  return getUserParameter();
 };
---
+++ src/infrastructures/dummy/parameterRepository.ts
@@ -0,0 +1,23 @@
+export const getUserParameter = () => {
+  return {
+    parameters: {
+      STR: { self: 10, mod: 0, tmp: 0 },
+      CON: { self: 11, mod: 0, tmp: 0 },
+      POW: { self: 12, mod: 0, tmp: 0 },
+      DEX: { self: 13, mod: 0, tmp: 0 },
+      APP: { self: 14, mod: 0, tmp: 0 },
+      SIZ: { self: 15, mod: 0, tmp: 0 },
+      INT: { self: 16, mod: 0, tmp: 0 },
+      EDU: { self: 17, mod: 0, tmp: 0 },
+      INC: { self: 18, mod: 0, tmp: 0 },
+    },
+    extraParams: {
+      HP: { self: 12, mod: 0, tmp: 0 },
+      MP: { self: 13, mod: 0, tmp: 0 },
+      SAN: { self: 14, mod: 0, tmp: 0 },
+      IDE: { self: 15, mod: 0, tmp: 0 },
+      LUK: { self: 16, mod: 0, tmp: 0 },
+      KNOW: { self: 17, mod: 0, tmp: 0 },
+    },
+  };
+};
