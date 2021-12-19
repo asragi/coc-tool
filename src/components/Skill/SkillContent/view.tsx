8ba338468a30bfeb203b6df16c291f1fec8d7f4a
Ragi <ragi-justaway@hotmail.co.jp>
2021-12-19 17:55:45 +0900

    deploy SkillListContent

--- src/components/Skill/SkillContent/view.tsx
+++ src/components/Skill/SkillContent/view.tsx
@@ -1,7 +1,8 @@
 import { Stack } from '@mui/material';
+import { SkillListContent } from '../SkillListContent';
 
 export interface SkillContentViewProps {}
 
 export const SkillContentView = ({}: SkillContentViewProps) => {
-  return <Stack>Content</Stack>;
+  return <Stack><SkillListContent /></Stack>;
 };
